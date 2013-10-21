b838e266f4967292c26ef921bc0d639b26ad6923
Anas Ambri <anas.ambri@radialpoint.com>
2013-10-21 10:48:27 -0400

    Refactored models, appleSearch.findAppByName only returns first result

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var async = require('async'),
     program = require('commander'),
     utils = require('./lib/utils.js'),
     query = require('./lib/query/'),
-    AppInfo = require('./lib/model/'),
+    models = require('./lib/model/'),
     appStore = require('./lib/appStore/'),
     playStore = require('./lib/playStore/');
 
@@ -13,44 +13,25 @@ var appVersion = require('./package.json').version;
 var config = require('./config/config.json');
 playStore.init(config);
 
+noodle.configure({
+    debug: false
+});
+
 program
     .version(appVersion)
     .usage('[options] <filename 1> <filename2> ...')
     .option('-u, --urls','Specify path to file with urls of websites')
     .parse(process.argv);
 
-var listApps = [];
-if(program.urls){
-    var listWebsites = [];
-    process.argv.forEach(function(item, index){
-	if(index >= 3) {
-	    try{
-		if(utils.endsWith(item, '.txt')){
-		    utils.loadFromTxt(listWebsites, utils.getRelativePath(item));
-		}
-		else if (utils.endsWith(item, '.json')){
-		    utils.loadFromJson(listWebsites, utils.getRelativePath(item));
-		}
-	    }
-	    catch(err){
-		console.log( "Could not open file "+item+" with error "+err );
-	    }
-	}
-    });
-
-    listWebsites.forEach(function(website){
-	listApps.push(new AppInfo(website));
-    });
-}
+var listApps = utils.loadWebsites();
 
 listApps.forEach(function(app){
     utils.getWebsiteName(app.websiteUrl, function(name){
 	app.websiteName = name;
 	try{
 	    appStore.findAppByName(app.websiteName, function(res){
-		var results = JSON.parse(res).results;
 		console.log( "*************Apple*************" );
-		console.log( results[0] );
+		console.log( res );
 	    });
 	}
 	catch(err){
